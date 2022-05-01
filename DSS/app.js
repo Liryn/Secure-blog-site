e4d319e34f6d5b87d621b728d18ed6ee4bf52b0a
Joe Ashman <80278812+joeashman1@users.noreply.github.com>
2022-05-01 20:55:45 +0100

    files
    ejs, app.js and css

---
+++ DSS/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+// const mysql = require('mysql')
+// const { redirect } = require('express/lib/response');
+// const res = require('express/lib/response');
+
+// const data = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '123',
+// });
+
+// data.connect((err) => {
+//     if(err){
+//         throw err;
+//     }
+//     console.log("mysql connected")
+// });
+
+const app = express(); 
+app.set('view engine', 'ejs');
+app.set('views', 'ejs');
+
+// app.get('/createdata', (req, res) =>{
+//     let sql = 'CREATE DATABASE nodemysql';
+//     data.query(sql, (err, result) =>{
+//         if(err) throw err;
+//         res.send('Database created');
+//     });
+// });
+
+app.listen(3000);
+
+app.get('/homepage', (req, res) => {
+    const blogs = [
+        {title: 'SAMPLE TITLE', content: 'SAMPLE WORDS'},
+        {title: 'SAMPLE TITLE', content: 'SAMPLE WORDS'},
+        {title: 'SAMPLE TITLE', content: 'SAMPLE WORDS'},
+    ];
+    res.render('homepage', {blogs});
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/createaccount', (req, res) => {
+    res.render('createaccount');
+});
+
+app.get('/makepost', (req, res) => {
+    res.render('makepost')
+});
+
+app.get('/myprofile', (req, res) => {
+    res.render('myprofile')
+});
+
+app.get('/settings', (req, res) => {
+    res.render('settings')
+});
+
+app.use(express.static('css'));
+app.use(express.urlencoded({ extended: true}))
+
+// app.post('/homepage', (req, res) =>{
+//     const blog = new Blog(req.body);
+//     blog.save()
+//         .then((result) =>{
+//             res.redirect('/homepage');
+//         })
+//         .catch((err) =>{
+//             console.log(err);
+//         })   });
