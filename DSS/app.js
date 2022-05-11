6616c3b88f036528e506af089642d9feb09a19e4
Joe Ashman <80278812+joeashman1@users.noreply.github.com>
2022-05-11 17:17:59 +0100

    Add files via upload

--- DSS/app.js
+++ DSS/app.js
@@ -1,35 +1,36 @@
 const express = require('express');
-// const mysql = require('mysql')
+const mysql = require('mysql');
+const bcrypt = require('bcryptjs');
+const { parseUrl } = require('mysql/lib/ConnectionConfig');
+
+// const blog = require('./models/blog');
 // const { redirect } = require('express/lib/response');
 // const res = require('express/lib/response');
+// const blog = require('./models/blog');
 
-// const data = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '123',
-// });
+const database = mysql.createConnection({
+        host: "sql4.freesqldatabase.com", 
+        user: "sql4491086", 
+        password: "k6TrSsVcap", 
+        database: "sql4491086",
+        port: 3306,
+});
 
-// data.connect((err) => {
-//     if(err){
-//         throw err;
-//     }
-//     console.log("mysql connected")
-// });
+database.connect((err) => {
+    if(err){
+        console.log('mysql connection failed');
+    } else {
+    console.log("mysql connected")
+    };
+});
 
 const app = express(); 
 app.set('view engine', 'ejs');
 app.set('views', 'ejs');
 
 app.use(express.static('css'));
-app.use(express.urlencoded({ extended: true}))
-
-// app.get('/createdata', (req, res) =>{
-//     let sql = 'CREATE DATABASE nodemysql';
-//     data.query(sql, (err, result) =>{
-//         if(err) throw err;
-//         res.send('Database created');
-//     });
-// });
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
 
 app.listen(3000);
 
@@ -60,16 +61,69 @@ app.get('/myprofile', (req, res) => {
 });
 
 app.get('/settings', (req, res) => {
-    res.render('settings')
+    const users = database.query('SELECT * FROM Users', (err, users) => {
+    res.render('settings', users)
+    });
 });
 
-app.post('/homepage', (req, res) =>{
-    const blog = req.body;
-    blogs.push(blog)
-        // .then((result) =>{
-        //     res.redirect('/homepage');
-        // })
-        // .catch((err) =>{
-        //     console.log(err);
-        // })   
-    });
+// app.post('/homepage', (req, res) =>{
+//     const blog = new blog(req.body);
+//     blog.save()
+//         .then((result) =>{
+//             res.redirect('/homepage');
+//         })
+//         .catch((err) =>{
+//             console.log(err);
+//         })   
+//     });
+
+app.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await database('Users').first('*').where({email: email});
+        if(user) {
+            const pass = await bcrypt.compare(password, user.hash);
+            if(pass) {
+                console.log('Valid credentials!');
+            }
+            else {
+                console.log('Invalid credentials')
+            }
+        }
+    } catch(err){
+        console.log(err);
+    }
+});
+
+app.post('/createaccount', async (req, res) => {
+    try {
+        const {fname, lname, email, password} = req.body;
+        console.log(req.body)
+
+        const hash = await bcrypt.hash(password, 8);
+        
+        // const hash = await genSalt().then(salt => {
+        //     bcrypt.hash(password, salt);
+        // });
+
+        console.log(hash);
+
+        database.query("INSERT INTO Users SET ?", {first_name: fname, second_name: lname, email: email, password: hash}), (err, res) =>{
+            if(err){
+                console.log(err);
+            } else {
+                console.log(res)
+            }
+        }
+    } 
+    catch(err) {
+        console.log(err);
+    }
+});   
+
+// bcrypt.genSalt(12).then(salt => { // Arg is the length of running operation (Salt Rounds). Higher equals more secure, but takes more time.
+//     bcrypt.hash("password", salt).then(hash => {
+//         // Store in password DB
+//         bcrypt.compare("password", hash).then(result => console.log(result));
+//     });
+// })
