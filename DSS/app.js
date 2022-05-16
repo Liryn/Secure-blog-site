0c2ce8624f6f0f6921805f8ffa43eb3ffc8ea5a2
Joe Ashman <80278812+joeashman1@users.noreply.github.com>
2022-05-16 13:24:57 +0100

    Add files via upload

--- DSS/app.js
+++ DSS/app.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const JSON = require('JSON');
 
 const { parseUrl } = require('mysql/lib/ConnectionConfig');
+const { indexOf } = require('lodash');
 
 dotenv.config({path: './db.env'});
 
@@ -29,13 +30,33 @@ app.set('view engine', 'ejs');
 app.set('views', 'ejs');
 
 app.use(express.static('css'));
-app.use(express.urlencoded({ extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 app.listen(3000);
 
 app.get('/homepage', (req, res) => {
     res.render('homepage');
+    let blogArray = [];
+    const blog = database.query("SELECT * FROM Blogs", (err, res) => {
+        if(err){
+            console.log(err);
+        } else {
+            // console.log(res);
+            for (let i = 0; i < res.length; i++){
+                //Change name of blogstuff
+                var blogListing = [];
+                var blogstuff = JSON.stringify(res[i])
+                var postID = (blogstuff.slice(blogstuff.indexOf("PostID")+8,blogstuff.indexOf("title")-2))
+                var title = (blogstuff.slice(blogstuff.indexOf("title")+8,blogstuff.indexOf("content")-3))
+                var content = (blogstuff.slice(blogstuff.indexOf("content")+10,blogstuff.indexOf("UserID")-3))
+                var userID = (blogstuff.slice(blogstuff.indexOf("UserID")+8,blogstuff.indexOf("}")))
+                blogListing.push(postID,title,content,userID)
+                blogArray.push(blogListing)
+            }
+            console.log(blogArray)
+        }
+    });
 });
 
 app.get('/login', (req, res) => {
@@ -62,16 +83,17 @@ app.get('/settings', (req, res) => {
 
 app.post('/homepage', async (req, res) => {
     try {
-        const {title, content} = req.body;
+        const {title, content, userid} = req.body;
         console.log(req.body)
-        database.query("INSERT INTO Blogs SET ?", {title: title, content: content}), (err, res) => {
+        database.query("INSERT INTO Blogs SET ?", {title: title, content: content, UserID: userid}), (err, res) => {
+            // redirect fix
             if(err){
                 console.log(err);
             } else {
-                res.redirect('/homepage')
                 console.log(res)
             }
         }
+        res.redirect('/homepage')
     } 
     catch(err) {
         console.log(err);
@@ -81,7 +103,6 @@ app.post('/homepage', async (req, res) => {
 app.post('/login', async (req, res) => {
 
     try {
-
         const {email, password} = req.body;
 
         if(!email){
@@ -90,29 +111,23 @@ app.post('/login', async (req, res) => {
             console.log('missing password')
         }
 
-        const hash = database.query("SELECT password FROM Users WHERE email = ?", [email]) 
+        const hash = database.query("SELECT password FROM Users WHERE email = ? LIMIT 1", [email])
+        console.log(hash);
 
         if(hash){
-
-        const check = async (password, hash) => {
-            return await bcrypt.compare(password, hash), (err, res) => {
-                if(res == true){
-                    console.log('nice')
-                } else {
-                    console.log('long')
-                }
-            }
-        }
-            if(check == true){
-                console.log('valid');
-                res.redirect('/makepost');
-            }
-            else {
-                console.log('invalid');
-            }
+            // const check = await bcrypt.compare(password, hash)
+        
+            // if(check == true){
+            //     console.log('valid');
+            //     res.redirect('/makepost');
+            // }
+            // else {
+            //     console.log('invalid');
+            // }
         } else {
             console.log('no user found');
         }
+
 
 } catch(err){
     console.log(err);
@@ -129,13 +144,15 @@ app.post('/createaccount', async (req, res) => {
                 console.log(err);
 			} 
 			// Check if the user table is empty
-			else if(res.length > 0) {				
+			else if(res.length > 0) {
+            console.log('email in use');
+            
 				// Turn the res array of emails to one large string, check if the email has an index in the string, if so, it's present
 				if(JSON.stringify(res).indexOf(email) >= 0){
 					console.log("Index is", JSON.stringify(res).indexOf(email));
 					console.log('Email in Use, has been found with the index checker');
 				}
-					// Find way to cancel process,
+					// Find way to cancel process
             }
         })
 
@@ -143,7 +160,8 @@ app.post('/createaccount', async (req, res) => {
             // find way to cancel process as per NIST convention
         }
 
-        const hash = await bcrypt.hash(password, 10); // generates salt
+        const salt = await bcrypt.genSalt(); // Generate salt
+        const hash = await bcrypt.hash(password, salt); // Generate hash
 
         database.query("INSERT INTO Users SET ?", {first_name: fname, second_name: lname, email: email, password: hash}), (err, res) =>{
             if(err){
@@ -158,4 +176,4 @@ app.post('/createaccount', async (req, res) => {
     catch(err) {
         console.log(err);
     }
-}); 
+});
