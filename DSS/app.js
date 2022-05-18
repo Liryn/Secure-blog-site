4f8d4da36692cb0b6a9cfa60a2bfb327097b156d
MaxHopwood <ksg18snu@uea.ac.uk>
2022-05-18 10:50:25 +0100

    Added validation and sanitisation to create account

--- DSS/app.js
+++ DSS/app.js
@@ -6,10 +6,13 @@ const JSON = require('JSON');
 const bodyParser = require('body-parser');
 const { parseUrl } = require('mysql/lib/ConnectionConfig');
 const { indexOf, join } = require('lodash');
+const { check, validationResult } = require('express-validator');
+
 
 var escapeHtml = require('escape-html')
 var session = require('express-session')
 var alert = require('alert');
+
 
 dotenv.config({path: './db.env'});
 
@@ -78,7 +81,7 @@ app.get('/makepost', (req, res) => {
 });
 
 app.get('/myprofile', (req, res) => {
-    database.query("SELECT * FROM Blogs WHERE userID = ?",[1], (err, blog) => { // change to account for users session id.
+    database.query("SELECT * FROM Blogs WHERE userID = ?",[1], (err, blog) => { // change to account for users session id. Will therefore not need mitigating
         if(err){
             console.log(err);
         } else {
@@ -96,6 +99,7 @@ app.post('/homepage', async (req, res) => {
     try {
         const {title, content, userid} = req.body;
         console.log(req.body)
+		//Ignore User ID as it should be taken automatically
         database.query("INSERT INTO Blogs SET ?", {title: title, content: content, UserID: userid}), (err, res) => {
             // redirect fix
             if(err){
@@ -116,20 +120,46 @@ app.post('/login', express.urlencoded({ extended: false}),async (req, res) => {
     try {
         const {email, password} = req.body;
 		
-		const sql =`SELECT password FROM Users WHERE email = '${email}' LIMIT 1`;
-		
 		var hash;
 		
 		function getPassword(callback) {
-			database.query(sql, (err, databaseHash) => {
-				if(err){
-					console.log(err);
-				}
-				else{
-					hash = databaseHash
-					callback();
-				}
-			});
+			
+			//This regex accepts (a-z, A-Z, 0-9), then 1 "@" for the email, then (a-z, A-Z and ".")
+			if (!email.match(/^[a-zA-Z0-9]+\@{1}[a-zA-Z/.]+$/)){
+				console.log("Email is in unacceptable format")
+				
+				//Refresh the create account page to clear boxes
+				res.render('login')
+
+				//alert('For Email please do not use any special characters, apart from the "@" and "." in your email address')
+				//End the post
+				res.end()
+			}
+			// This regex accepts everything except "=", "'", '"' and ";"
+			// NEED TO SANITISE THIS INSTEAD
+			else if (!password.match(/^[^\=\'\"\;\<\>]+$/)){
+				console.log("Password is in unacceptable format")
+				
+				//Refresh the create account page to clear boxes
+				res.render('login')
+
+				//alert("For Password please do not use any of the following characters; (=), ('), (\"), (;), (<), (>)")
+				//End the post
+				res.end()
+			}
+			
+			else{
+				database.query("SELECT password FROM Users WHERE email = ?", email, (err, databaseHash) => {
+					if(err){
+						console.log(err);
+					}
+					else{
+						hash = databaseHash
+						
+						callback();
+					}
+				});
+			}
 		}
 		
 		async function compareHash() {
@@ -193,71 +223,128 @@ app.get('/logout', function (req, res, next) {
   })
   
 
-
-app.post('/createaccount', async (req, res) => {
+// Sanitisation
+var createAccountSanitizer = [
+	//Validates and Sanitizes Email
+	check('email').isEmail().trim().escape().normalizeEmail(),
+    //Sanitizes Password
+	check('password').trim().escape()];
+  
+  
+app.post('/createaccount', createAccountSanitizer, async (req, res) => {
     try {
-		//Obtain variables from user input in req
-        const {fname, lname, email, password} = req.body;
-		//Initiate a boolean var, true if email is new, false if it already has been used
-		var newEmail;
-		
-		//Function to check new email against list of existing ones
-		function checkEmail(callback){
+		const errors = validationResult(req);
+		//Any Errors will come from isEmail() and so means not inputted valid email
+		if (!errors.isEmpty()) {
+			res.render('createaccount')
+			alert('To input Email, please do not use special characters. Format email such as: "Example10@gmail.com"')
+			//End the post
+			res.end()
+		}
+		else {
+			//Obtain variables from user input in req
+			const {fname, lname, email, password} = req.body;
+
+			//Initiate a boolean var, true if email is new, false if it already has been used
+			var newEmail;
 			
-			// Get all emails in the database
-			database.query("SELECT email FROM Users", (err, res) => {
-				if(err) {
-					console.log(err);
-				} 
+			//Function to check new email against list of existing ones
+			function checkEmail(callback){
 				
-				// Check if there are any emails in the database
-				else if(res.length > 0) {
+				//This regex accepts (a-z, A-Z and whitespace), then 1 "-" for double barrel names, then (a-z and A-Z)
+				if(!fname.match(/^[a-zA-Z\s]+\-{0,1}[a-zA-Z]+$/)){
+					console.log("First Name is in unacceptable format")
 					
-					// Turn the res array of emails to one large string, check if the email has an index in the string, if so, it's present
-					if(JSON.stringify(res).indexOf(email) >= 0){
-						console.log('Email in Use, has been found with the index checker');
-						newEmail = false;
-					}
-					else{
-						newEmail = true;
-					} 
-				callback();
+					//Refresh the create account page to clear boxes
+					res.render('createaccount')
+
+					alert('To input First Name, please use only letters. The Following formats are accepted; "Elizabeth", "Elizabeth Ann", "Elizabeth-Ann"')
+					//End the post
+					res.end()
 				}
-			})
-		}
+				//This regex accepts (a-z, A-Z and whitespace), then 1 "-" for double barrel names, then (a-z and A-Z)
+				else if (!lname.match(/^[a-zA-Z\s]+\-{0,1}[a-zA-Z]+$/)){
+					console.log("Last Name is in unacceptable format")
+					
+					//Refresh the create account page to clear boxes
+					res.render('createaccount')
+
+					alert('To input Last Name, please use only letters. The Following formats are accepted; "Smith", "Smith Jones", "Smith-Jones"')
+					//End the post
+					res.end()
+				}
+				//This regex accepts (a-z, A-Z, 0-9), then 1 "@" for the email, then (a-z, A-Z and ".")
+				else if (!email.match(/^[a-zA-Z0-9]+\@{1}[a-zA-Z/.]+$/)){
+					console.log("Email is in unacceptable format")
+					
+					//Refresh the create account page to clear boxes
+					res.render('createaccount')
+
+					alert('To input Email, please do not use special characters. Format email such as: "Example10@gmail.com"')
+					//End the post
+					res.end()
+				}
+				// This regex finds all special characters except for "_"
+				else {
+					console.log("Email after sanitization: ", email)
+					console.log("Password after sanitization: ", password)
 		
-		async function processAccount(){
-			//Check if the email already exists
-			if(newEmail == false){
-				console.log("Process of making account is ending, account should not have been added")
-				//Refresh the create accoun page to clear boxes
-				res.render('createaccount')
-
-				alert('That email has already been registered, please try again')
-				//End the post
-				res.end()
-			}
-			else{
-				//If email is new, create a salt and hash
-				console.log("Email is absolutely fine, it can be added to the database");
-				const salt = await bcrypt.genSalt(10); // Generate salt
-				const hash = await bcrypt.hash(password, salt); // Generate hash
-
-				//Use an SQL inset statement to place the user into the table
-				database.query("INSERT INTO Users SET ?", {first_name: fname, second_name: lname, email: email, password: hash}), (err, res) =>{
-					if(err){
-						console.log(err);
-					} else {
-						console.log(res);
+					// Get all emails in the database
+					database.query("SELECT email FROM Users", (err, res) => {
+						if(err) {
+							console.log(err);
+						} 
+						
+						// Check if there are any emails in the database
+						else if(res.length > 0) {
+							
+							// Turn the res array of emails to one large string, check if the email has an index in the string, if so, it's present
+							if(JSON.stringify(res).indexOf(email) >= 0){
+								console.log('Email in Use, has been found with the index checker');
+								newEmail = false;
+							}
+							else{
+								newEmail = true;
+							} 
+						callback();
 						}
-					}
-				console.log("REGISTERED");
-				res.redirect('/login');
+					})
+				}
 			}
 			
-		}
-		checkEmail(processAccount);
-    } 
+			async function processAccount(){
+				//Check if the email already exists
+				if(newEmail == false){
+					console.log("Process of making account is ending, account should not have been added")
+					//Refresh the create account page to clear boxes
+					res.render('createaccount')
+
+					alert('That email has already been registered, please try again')
+					//End the post
+					res.end()
+				}
+				else{
+					//If email is new, create a salt and hash
+					console.log("Email is absolutely fine, it can be added to the database");
+					const salt = await bcrypt.genSalt(10); // Generate salt
+					const hash = await bcrypt.hash(password, salt); // Generate hash
+
+					//Use an SQL inset statement to place the user into the table
+					database.query("INSERT INTO Users SET ?", {first_name: fname, second_name: lname, email: email, password: hash}), (err, res) =>{
+						if(err){
+							console.log(err);
+						} else {
+							console.log(res);
+							}
+						}
+					console.log("REGISTERED");
+					res.redirect('/login');
+				}
+				
+			}
+			checkEmail(processAccount);
+		} 
+	}
     catch(err) {
         console.log(err);
     }
