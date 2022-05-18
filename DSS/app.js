61f9d19ddfae379c841e91184bce4467103d492a
Samuel Bedeau <sambedeau@gmail.com>
2022-05-18 12:29:15 +0100

    Add files via upload

--- DSS/app.js
+++ DSS/app.js
@@ -9,7 +9,6 @@ const { indexOf, join } = require('lodash');
 
 var escapeHtml = require('escape-html')
 var session = require('express-session')
-var alert = require('alert');
 
 dotenv.config({path: './db.env'});
 
@@ -28,34 +27,38 @@ database.connect((err) => {
     console.log("mysql connected")
     };
 });
-
+var useremail = "";
 const app = express(); 
 app.set('view engine', 'ejs');
 app.set('views', 'ejs');
 
 app.use(express.static('css'));
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 app.listen(3000);
 
 app.use(session({
     secret: 'keyboard cat',
-    resave: false,
+    resave: true,
     saveUninitialized: true
   }))
 
   function isAuthenticated (req, res, next) {
-    if (req.session.user) next()
+    if (useremail) next()
     else next('route')
   }
-  app.get('/', isAuthenticated, function (req, res) {
+  
+  app.get('/welcomepage', isAuthenticated, function (req, res) {
     // this is only called when there is an authentication user due to isAuthenticated
-    res.send('hello, ' + escapeHtml(req.session.user) + '!' +
-      ' <a href="/login">Logout</a>')
+    res.send('hello, ' + escapeHtml(useremail) + '!' +
+      ' <a href="/myprofile">Welcome</a>')
   })
+  
 
-app.get('/homepage', (req, res) => {
+app.get('/homepage', function(req, res) {
+    // If the user is loggedin
+    
     database.query("SELECT * FROM Blogs", (err, blog) => {
         if(err){
             console.log(err);
@@ -131,6 +134,22 @@ app.post('/login', express.urlencoded({ extended: false}),async (req, res) => {
 				}
 			});
 		}
+       
+        req.session.regenerate(function (err) {
+            if (err) next(err)
+        
+            // store user information in session, typically a user id
+            req.session.user = req.body.email
+            useremail = req.session.user
+            // save the session before redirection to ensure page
+            // load does not happen before session is saved
+            req.session.save(function (err) {
+              if (err) return next(err)
+              //res.redirect('/homepage')
+              console.log("session saved")
+              console.log("saved session username is: " + useremail)
+            })
+          })
 		
 		async function compareHash() {
 			stringPassword = JSON.stringify(hash)
@@ -145,7 +164,7 @@ app.post('/login', express.urlencoded({ extended: false}),async (req, res) => {
 			
 			if(hashComparison){
                 console.log('Bcrypt says the passwords match');
-                res.redirect('/myprofile');
+                res.redirect('/welcomepage');
             }
             else {
                 console.log('Bcrypt says the passwords do not match');
@@ -156,109 +175,69 @@ app.post('/login', express.urlencoded({ extended: false}),async (req, res) => {
 	catch(err){
     console.log(err);
     }
-     // regenerate the session, which is good practice to help
-  // guard against forms of session fixation
-  req.session.regenerate(function (err) {
-    if (err) next(err)
-
-    // store user information in session, typically a user id
-    req.session.user = req.body.user
-
-    // save the session before redirection to ensure page
-    // load does not happen before session is saved
-    req.session.save(function (err) {
-      if (err) return next(err)
-      res.redirect('/')
-    })
-  })
+    
 });
-
-app.get('/logout', function (req, res, next) {
-    // logout logic
-  
-    // clear the user from the session object and save.
-    // this will ensure that re-using the old session id
-    // does not have a logged in user
-    req.session.user = null
-    req.session.save(function (err) {
-      if (err) next(err)
-  
-      // regenerate the session, which is good practice to help
-      // guard against forms of session fixation
-      req.session.regenerate(function (err) {
-        if (err) next(err)
-        res.redirect('/')
-      })
-    })
-  })
-  
-
 
 app.post('/createaccount', async (req, res) => {
     try {
-		//Obtain variables from user input in req
         const {fname, lname, email, password} = req.body;
-		//Initiate a boolean var, true if email is new, false if it already has been used
-		var newEmail;
-		
-		//Function to check new email against list of existing ones
-		function checkEmail(callback){
-			
-			// Get all emails in the database
-			database.query("SELECT email FROM Users", (err, res) => {
-				if(err) {
-					console.log(err);
+
+        // Get all emails in the database
+        database.query("SELECT email FROM Users", (err, res) => {
+            if(err) {
+                console.log(err);
+			} 
+			// Check if the user table is empty
+			else if(res.length > 0) {
+            console.log('There are emails');
+            
+				// Turn the res array of emails to one large string, check if the email has an index in the string, if so, it's present
+				if(JSON.stringify(res).indexOf(email) >= 0){
+					console.log("Index is", JSON.stringify(res).indexOf(email));
+					console.log('Email in Use, has been found with the index checker');
 				} 
-				
-				// Check if there are any emails in the database
-				else if(res.length > 0) {
-					
-					// Turn the res array of emails to one large string, check if the email has an index in the string, if so, it's present
-					if(JSON.stringify(res).indexOf(email) >= 0){
-						console.log('Email in Use, has been found with the index checker');
-						newEmail = false;
-					}
-					else{
-						newEmail = true;
-					} 
-				callback();
-				}
-			})
-		}
-		
-		async function processAccount(){
-			//Check if the email already exists
-			if(newEmail == false){
-				console.log("Process of making account is ending, account should not have been added")
-				//Refresh the create accoun page to clear boxes
-				res.render('createaccount')
 
-				alert('That email has already been registered, please try again')
-				//End the post
-				res.end()
-			}
-			else{
-				//If email is new, create a salt and hash
-				console.log("Email is absolutely fine, it can be added to the database");
-				const salt = await bcrypt.genSalt(10); // Generate salt
-				const hash = await bcrypt.hash(password, salt); // Generate hash
+                // Find way to cancel process
+            }
+        })
 
-				//Use an SQL inset statement to place the user into the table
-				database.query("INSERT INTO Users SET ?", {first_name: fname, second_name: lname, email: email, password: hash}), (err, res) =>{
-					if(err){
-						console.log(err);
-					} else {
-						console.log(res);
-						}
-					}
-				console.log("REGISTERED");
-				res.redirect('/login');
-			}
-			
-		}
-		checkEmail(processAccount);
+        if(password < 8){
+            // find way to cancel process as per NIST convention
+        }
+
+        const salt = await bcrypt.genSalt(10); // Generate salt
+        const hash = await bcrypt.hash(password, salt); // Generate hash
+
+        database.query("INSERT INTO Users SET ?", {first_name: fname, second_name: lname, email: email, password: hash}), (err, res) =>{
+            if(err){
+                console.log(err);
+            } else {
+                console.log(res);
+                }
+            }
+        console.log("REGISTERED");
+        res.redirect('/login');
     } 
     catch(err) {
         console.log(err);
     }
 });
+
+app.get('/logout', function (req, res, next) {
+  // logout logic
+
+  // clear the user from the session object and save.
+  // this will ensure that re-using the old session id
+  // does not have a logged in user
+  req.session.user = null
+  req.session.save(function (err) {
+    if (err) next(err)
+
+    // regenerate the session, which is good practice to help
+    // guard against forms of session fixation
+    req.session.regenerate(function (err) {
+      if (err) next(err)
+      res.redirect('/')
+    })
+  })
+})
